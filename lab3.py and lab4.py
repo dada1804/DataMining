2050dc1a9005464e61fd478a8cf38853e69d9a47
Venkatesh <124506162+dada1804@users.noreply.github.com>
2023-07-11 13:03:15 +0530

    Train Decision Tree Model

--- lab3.py and lab4.py
+++ lab3.py and lab4.py
@@ -1,4 +1,4 @@
-import pandas as pd
+ import pandas as pd
 from sklearn.tree import DecisionTreeClassifier,plot_tree
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
