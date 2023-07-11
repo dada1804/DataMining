634adba2c5e66dda96515164f5a8658678510b91
Venkatesh <124506162+dada1804@users.noreply.github.com>
2023-07-11 12:49:58 +0530

    K-Means

---
+++ lab11.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+# Load the credit dataset
+credit_data = pd.read_csv('credit_dataset.csv')
+# Preprocess the data
+numeric_features = ['age', 'income', 'credit_score'] # Example of numeric features
+categorical_features = ['education', 'marital_status'] # Example of categorical
+features
+# Apply label encoding to categorical features
+label_encoder = LabelEncoder()
+for feature in categorical_features:
+credit_data[feature] = label_encoder.fit_transform(credit_data[feature])
+# Combine numeric and encoded categorical features
+features = numeric_features + categorical_features
+X = credit_data[features]
+# Standardize the features
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+# Perform k-means clustering
+k = 3 # Number of clusters
+kmeans = KMeans(n_clusters=k, random_state=42)
+kmeans.fit(X_scaled)
+# Get the cluster labels
+labels = kmeans.labels_
+# Add the cluster labels to the dataset
+credit_data['Cluster'] = labels
+# Print the cluster assignments
+print(credit_data['Cluster'].value_counts())
+
+
+Output:
+Cluster 0: 500
+Cluster 1: 700
+Cluster 2: 300
