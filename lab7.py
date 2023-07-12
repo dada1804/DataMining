613908ae81dd5210ee880bbafd5c6f41b2d65d3b
Venkatesh <124506162+dada1804@users.noreply.github.com>
2023-07-12 19:06:54 +0530

    Update lab7.py

--- lab7.py
+++ lab7.py
@@ -1,30 +1,24 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score,train_test_split
 from sklearn.preprocessing import LabelEncoder
-# Load the dataset from CSV
-dataset = pd.read_csv('credit-g_csv.csv')
-label_encoder = LabelEncoder()
+
+dataset=pd.read_csv('credit-g_csv.csv')
+
+label_encoder=LabelEncoder()
 for column in dataset.columns:
-if dataset[column].dtype == 'object':
-dataset[column] = label_encoder.fit_transform(dataset[column])
-# Separate the features (X) and target variable (y)
-X = dataset.drop('class', axis=1)
-y = dataset['class']
-# Create a decision tree classifier
-decision_tree = DecisionTreeClassifier()
-# Perform cross-validation and get the accuracy scores
-cv_scores = cross_val_score(decision_tree, X, y, cv=5)
-# Fit the decision tree on the entire dataset
-decision_tree.fit(X, y)
-# Print the decision tree
-print(decision_tree)
-# Print the cross-validation results
-print("Cross-Validation Accuracy Scores:", cv_scores)
-print("Mean Accuracy:", cv_scores.mean())
+    if dataset[column].dtype=='object':
+        dataset[column]=label_encoder.fit_transform(dataset[column])
 
+X=dataset.drop('class',axis=1)
+y=dataset['class']
 
-Output:
-DecisionTreeClassifier()
-Cross-Validation Accuracy Scores: [0.655 0.745 0.665 0.695 0.735]
-Mean Accuracy: 0.699
+model=DecisionTreeClassifier()
+
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
+cross=cross_val_score(model,X,y,cv=5)
+model.fit(X_train,y_train)
+treeacc=model.score(X_test,y_test)
+print("DecisionTree accuracy:",treeacc)
+print("Cross val accuracy:",cross)
+print("Avg of cross val accuracy:",cross.mean())
